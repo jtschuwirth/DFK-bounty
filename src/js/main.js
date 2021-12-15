0775b488ea6731238f1a75636092963a4b85979d
jtschuwirth <jtschuwirth@uc.cl>
2021-12-14 22:46:54 -0300

    seleccion de dias de busqueda

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import DayPicker from "react-day-picker";
+import DayPickerInput from "react-day-picker/DayPickerInput";
+
 const BN = require('bn.js');
 const Web3 = require("web3");
 
@@ -12,14 +15,25 @@ window.onload = async function initialize() {
         document.getElementById('app'));
 }
 
-
 function App() {
     const [currentMenu, setMenu] = useState("mainmenu");
+    const [currentAddress, setAddress] = useState("");
+    const[currentStartDay, setStartDay] = useState("");
+    const[currentEndDay, setEndDay] = useState("");
 
     return (
         <div>
-            <NavBar setMenu={setMenu} />
-            <Menu currentMenu={currentMenu}/>
+            <NavBar setMenu={setMenu} 
+            currentAddress={currentAddress}
+            currentStartDay={currentStartDay}
+            currentEndDay={currentEndDay}
+            />
+            <Menu currentMenu={currentMenu} 
+            currentAddress={currentAddress} 
+            setAddress={setAddress} 
+            setStartDay={setStartDay}
+            setEndDay={setEndDay}
+            />
         </div>
     );
 
@@ -38,24 +52,74 @@ function NavBar(props) {
     return (
         <div className = "navbar">
             <NavTitle id = {"mainmenu"} text={"DFK Transaction History"} setMenu={props.setMenu}/>
-            <NavButton id = {"menu1"} text={"menu 1"} setMenu={props.setMenu}/>
+            <NavButton id = {"address"} text={`current address: ${props.currentAddress}`} setMenu={props.setMenu}/>
+            <NavButton id = {"address"} text={`From: ${props.currentStartDay}`} setMenu={props.setMenu}/>
+            <NavButton id = {"address"} text={`To: ${props.currentEndDay}`} setMenu={props.setMenu}/>
         </div>
     )
 
 }
 
-
 function Menu(props) {
+    const[currentValue, setValue] = useState("");
+
+
+    function handleChange(event) {
+      setValue(event.target.value);
+    }
+
+    async function handleSubmit(event) {
+      setTransaction([])
+      event.preventDefault();
+      props.setAddress(currentValue);
+    }
+    function startInput(event) {
+      let unixTime = new Date(event).getTime() / 1000
+      props.setStartDay(unixTime)
+
+    }
+    function endInput(event) {
+      let unixTime = new Date(event).getTime() / 1000
+      props.setEndDay(unixTime)
+    }
+
     if (props.currentMenu == "mainmenu") {
         return (
             <div className = "menu">
                 <ul >
                     <div className="center"><h2>Transaction History</h2></div>
                     <br></br>
+                    <div className="center">From:</div>
+                    <div className="center"><DayPickerInput placeholder="DD/MM/YYYY" format="DD/MM/YYYY" onDayChange={startInput} /></div>
+                    <div className="center">To:</div>
+                    <div className="center"><DayPickerInput placeholder="DD/MM/YYYY" format="DD/MM/YYYY" onDayChange={endInput}/></div>
+                    <br></br>
+                    <div className="center">
+                    <form onSubmit={handleSubmit}>
+                      <label>
+                        Address: <input type="text" value={currentValue} onChange={handleChange} />
+                      </label>
+                      <input type="submit" value="Submit"/>
+                    </form>
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
                 </ul>
             </div>
         )
-    } else if (props.currentMenu == "menu2") {
+    } else if (props.currentMenu == "address") {
         return (
             <div className = "menu">
                 <ul >
@@ -67,6 +131,8 @@ function Menu(props) {
         )
     }
 }
+
+
 
 
 
